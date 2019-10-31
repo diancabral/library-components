92f8733bafc7ac63b7670c846597bf5d509b6c5a
Luiz Gustavo Dias Gonçalves <luiz.gustavo@am4.combr>
2019-10-30 21:59:02 -0300

    checkpoint

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+
 import './App.css';
 
+
+import AppInput from './components/input/input';
+import AppButton from './components/button/button';
+
 function App() {
+
+  const [var1, setVar1] = useState('');
+  const [var2, setVar2] = useState('');
+  const [var3, setVar3] = useState(0);
+
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div className="Box">
+
+        <h3>Inputs</h3>
+
+        <div className="box__space">
+      
+          <AppInput label="Label" type="text" value={var1} change={(e) => setVar1(e)} />
+
+        </div>
+
+        <div className="box__space">
+
+          <AppInput label="Label" type="text" error="Campo inválido" value={var2} change={(e) => setVar2(e)} />
+
+        </div>
+
+      </div>      
+
+      <div className="Box">
+
+        <h3>Buttons</h3>
+
+        <div className="box__space">
+      
+          <AppButton label="Enviar" color="blue" click={() => setVar3(var3 + 1)} />
+
+        </div>
+
+        <div className="box__space">
+
+          <AppButton label="Cancelar" color="red" click={() => setVar3(var3 + 1)} />
+
+        </div>
+
+        { var3 } cliques
+
+      </div>
+      
+    
     </div>
+
   );
 }
 
